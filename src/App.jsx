70698d1f6f8fa539005e8e47e64bd0fa2dc995e5
Juanes-xd <juanestebanortizbejarano@gmail.com>
2022-01-12 23:58:58 -0500

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Inicio from './components/Inicio';
+import Login from './components/Login';
+import Admin from './components/Admin';
 
 function App() {
   return (
-    <div>
+    <div className='container'>
+      <Router>
+
+      <Routes>
+      <Route exact path='/' element={<Inicio/>}></Route>
+      <Route path='/admin' element={<Admin/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      </Routes>
+
+      </Router>
       
     </div>
   );
