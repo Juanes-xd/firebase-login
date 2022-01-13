a06f5c99fccf185ab6aa03124bbdbcb1f671ed2c
Juanes-xd <juanestebanortizbejarano@gmail.com>
2022-01-13 16:18:18 -0500

    Empezando login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,12 +4,16 @@ import {Link} from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <div></div>
-            <div>
-
+        <div className='row mt-5'>
+            <div className='col'></div>
+            <div className='col'>
+                <form className='form-group'>
+                    <input className='form-control' placeholder='Introduce el email' type="text" />
+                    <input className='form-control mt-4' placeholder='Password' type="text" />
+                    <input value='Registrar usuario' type="submit" />
+                </form>
             </div>
-            <div></div>
+            <div className='col'></div>
         </div>
     )
 }
