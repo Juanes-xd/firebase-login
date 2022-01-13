1e090eacb1773d2157d2db6e3e2bafc5b638e213
Juanes-xd <juanestebanortizbejarano@gmail.com>
2022-01-13 15:14:54 -0500

    commit menu y boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Inicio from './components/Inicio';
 import Login from './components/Login';
 import Admin from './components/Admin';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className='container'>
       <Router>
-
+      <Menu></Menu>
       <Routes>
       <Route exact path='/' element={<Inicio/>}></Route>
       <Route path='/admin' element={<Admin/>}></Route>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'boostrap/package.json';
 import App from './App';
 
 
